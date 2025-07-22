6a9c556279681322dd71fc003112e0f87e5c48a0
Anwas coder <187599329+ANWAS-coder@users.noreply.github.com>
2025-07-22 16:29:12 +0700

    asd

---
+++ class_1.py
@@ -0,0 +1,14 @@
+#x = int(input("x = "))
+#y = int(input("y = "))
+#print(x+y)
+
+x = int(input("x = "))
+y = int(input("y = "))
+if x > y :
+    print("x > y")
+elif x < y :
+    print("x < y")
+elif x == y :
+    print("x = y")
+else :
+    print("idk")
---
+++ class_2.py
@@ -0,0 +1,16 @@
+x = int(input("x = "))
+y = int(input("y = "))
+if x > y :
+    print("x > y")
+    z = x * y
+    print(z)
+elif x < y :
+    print("x < y")
+    z = x/y
+    print(z)
+elif x == y :
+    print("x = y")
+    z = x+y
+    print(z)
+else :
+    print("idk")
